7b16a730131bbe44b0e8a53f84b2d1f97ca74fed
Shaye Ellerbe <slellerbe23@gmail.com>
2023-01-15 18:26:38 -0500

    edited Medium

--- Medium/script.js
+++ Medium/script.js
@@ -1,12 +1,12 @@
+const monthNumber = prompt("Enter a number between 1 and 12:");
+
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-function getMonth(num) {
-  if (num < 1 || num > 12) {
+if (monthNumber < 1 || monthNumber > 12) {
     console.log("Invalid input. Please enter a number between 1 and 12.");
   } else {
-    console.log(`${months[num - 1]}`);
+    console.log(`${months[monthNumber - 1]}`);
   }
-}
 
-const num = 3;
-getMonth(num);
+// const num = 3;
+// getMonth(num);
